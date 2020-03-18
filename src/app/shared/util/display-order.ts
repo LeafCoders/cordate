b5e596142955f495e2844452b61c680413152498
Oskar Bilén <oskar@ebilen.se>
2020-03-18 20:27:31 +0100

    Bug fixes with slide display order. Version 0.20

--- src/app/shared/util/display-order.ts
+++ src/app/shared/util/display-order.ts
@@ -1,6 +1,7 @@
 import { HasDisplayOrder } from '../server/rest-api.model';
 
 export function moveItemByDisplayOrder(items: Array<HasDisplayOrder>, moveItem: HasDisplayOrder, toItem: HasDisplayOrder): void {
+  const newDisplayOrder: number = toItem.displayOrder;
   items.forEach(item => {
     if (moveItem.displayOrder < toItem.displayOrder) {
       if (item.displayOrder >= moveItem.displayOrder && item.displayOrder <= toItem.displayOrder) {
@@ -12,6 +13,5 @@ export function moveItemByDisplayOrder(items: Array<HasDisplayOrder>, moveItem: 
       }
     }
   });
-  moveItem.displayOrder = toItem.displayOrder;
-  items.sort((a, b) => a.displayOrder > b.displayOrder ? 1 : -1);
+  moveItem.displayOrder = newDisplayOrder;
 }
--- src/app/slide/slide-list.component.ts
+++ src/app/slide/slide-list.component.ts
@@ -1,7 +1,6 @@
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
 import { BaseList } from '../shared/base/base-list';
-import { ItemsGroup, itemsGrouper } from '../shared/items-grouper';
 import { Slide, SlideList, SlideShow } from '../shared/server/rest-api.model';
 import { SlidesResource } from '../shared/server/slides.resource';
 import { moveItemByDisplayOrder } from '../shared/util/display-order';
@@ -14,7 +13,8 @@ import { moveItemByDisplayOrder } from '../shared/util/display-order';
 export class SlideListComponent extends BaseList<Slide> {
 
   currentSlideShow: SlideShow;
-  slideGroups: Array<ItemsGroup<Slide>> = [{ title: 'Kommer att visas eller visas nu', items: [] }, { title: 'Visas inte längre', items: [] }];
+  activeSlides: Array<Slide> = [];
+  passedSlides: Array<Slide> = [];
 
   constructor(
     private slidesResource: SlidesResource,
@@ -36,29 +36,21 @@ export class SlideListComponent extends BaseList<Slide> {
 
   public slideDrop(event: CdkDragDrop<Slide, Slide>): void {
     const moveSlide: Slide = event.item.data;
-    const toSlide: Slide = this.slideGroups[0].items[event.currentIndex];
+    const toSlide: Slide = this.activeSlides[event.currentIndex];
+    if (moveSlide === toSlide) {
+      return;
+    }
 
     // Perform move at server
     this.slidesResource.moveSlide(moveSlide, toSlide.id).subscribe();
 
     // Perform move at client (until updated slides are returned from server)
-    moveItemByDisplayOrder(this.slideGroups[0].items, moveSlide, toSlide);
+    moveItemByDisplayOrder(this.activeSlides, moveSlide, toSlide);
+    this.activeSlides.sort((a, b) => a.displayOrder > b.displayOrder ? -1 : 1);
   }
 
   private groupSlides(slides: SlideList): void {
-    let groups = itemsGrouper<Slide>(
-      (item: Slide) => (item.endTime && item.endTime.isBefore()) ? 1 : 0,
-      (item: Slide) => {
-        return {
-          title: undefined,
-          data: undefined,
-          items: []
-        };
-      },
-      (item: Slide) => item,
-      slides
-    );
-    this.slideGroups[0].items = groups[0] ? groups[0].items.sort((a, b) => a.displayOrder > b.displayOrder ? 1 : -1) : [];
-    this.slideGroups[1].items = groups[1] ? groups[1].items : [];
+    this.activeSlides = slides.filter(s => !s.isAfterActive()).sort((a, b) => a.displayOrder > b.displayOrder ? -1 : 1);
+    this.passedSlides = slides.filter(s => s.isAfterActive()).sort((a, b) => a.displayOrder > b.displayOrder ? 1 : -1);
   }
 }
